f0f8c06708b0c4d009ecdf4e448e454e383b5a41
Michał Wołek <32792800+Flaaj@users.noreply.github.com>
2022-10-08 14:20:32 +0200

    init

---
+++ src/components/LeftSidebar/NestedNav.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+type NavItems = {
+  [key: string]: NavItems | string;
+}[];
+
+interface INestedNav {
+  pages: NavItems;
+  headingLevel: number;
+  heading?: string;
+}
+
+function NestedNav({ pages, headingLevel = 2, heading }: INestedNav) {
+  const [showNav, setShowNav] = useState(true);
+
+  return (
+    <>
+      {heading && (
+        <button
+          onClick={() => setShowNav((prev) => !prev)}
+          className="px-3 rounded-md w-full block text-left hover:bg-blue-900"
+        >
+          <NavHeading headingLevel={headingLevel}>{heading}</NavHeading>
+        </button>
+      )}
+      {showNav && (
+        <ul className="mx-1">
+          {pages.map((page) => {
+            const [path, children] = Object.entries(page)[0]; // page has only one property
+            const isFile = typeof children === "string";
+
+            const childNavLevel = headingLevel + 1;
+            const childNavHeading = isNaN(parseInt(path)) && path.split("/").at(-1);
+
+            const isIndexFile = path.includes("index");
+            const linkHref = !isIndexFile && path.split(".").slice(0, -1)[0];
+
+            if (isFile && isIndexFile) return null;
+
+            const isEmptyDirectory = !isFile && Object.entries(children).length === 0;
+            if (isEmptyDirectory) return null;
+
+            return (
+              <li key={path}>
+                {isFile ? ( //
+                  <a href={linkHref} className="inline-block my-2">{path.split("/").at(-1)}</a>
+                ) : (
+                  <NestedNav //
+                    pages={children}
+                    headingLevel={childNavLevel}
+                    heading={childNavHeading}
+                  />
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default NestedNav;
+
+interface INavHeading {
+  headingLevel: number;
+  children: string;
+}
+const NavHeading = ({ headingLevel, children }: INavHeading) => {
+  switch (headingLevel) {
+    case 2:
+      return <h2 className="pl-0 my-2">{children}</h2>;
+    case 3:
+      return <h3 className="pl-0 my-2">{children}</h3>;
+    case 4:
+      return <h4 className="pl-0 my-2">{children}</h4>;
+    case 5:
+      return <h5 className="pl-0 my-2">{children}</h5>;
+    case 6:
+      return <h6 className="pl-0 my-2">{children}</h6>;
+    default:
+      return null;
+  }
+};
